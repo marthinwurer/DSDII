1877caec28b74a6997f052b0e7ef56257aada6b3
Benjamin Maitland <marthinwurer@gmail.com>
2018-01-28 21:43:11 -0500

    added some links to the README and added a script.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # DSDII
+
+
+resources:
+
+https://forum.digilentinc.com/topic/157-nexys-3-getting-started/
+
+https://reference.digilentinc.com/reference/software/adept/start
+
+[Xilinx Download](https://www.xilinx.com/support/download/index.html/content/xilinx/en/downloadNav/design-tools/v2012_4---14_7.html)
+
+https://lvoudouris.com/installing-xilinx-vivado-2016-4-and-intel-modelsim-starter-edition-16-1-on-64-bit-ubuntu-16-10-2/
+
--- lab2/pa.fromNetlist.tcl
+++ lab2/pa.fromNetlist.tcl
@@ -1,10 +1,10 @@
 
 # PlanAhead Launch Script for Post-Synthesis floorplanning, created by Project Navigator
 
-create_project -name lab2 -dir "C:/Users/bhm3791/Documents/DSDII/lab2/planAhead_run_2" -part xc6slx16csg324-3
+create_project -name lab2 -dir "/home/benjamin/Documents/classes/DSD1/lab2/planAhead_run_3" -part xc6slx16csg324-3
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "C:/Users/bhm3791/Documents/DSDII/lab2/lab1top.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {C:/Users/bhm3791/Documents/DSDII/lab2} }
+set_property edif_top_file "/home/benjamin/Documents/classes/DSD1/lab2/lab1top.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {/home/benjamin/Documents/classes/DSD1/lab2} }
 set_property target_constrs_file "lab1top.ucf" [current_fileset -constrset]
 add_files [list {lab1top.ucf}] -fileset [get_property constrset [current_run]]
 link_design
